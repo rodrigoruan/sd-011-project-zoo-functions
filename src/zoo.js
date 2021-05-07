d14ef65b1adb9f29091a81a9b911daf572315e13
Júlio Mota <juliomotacampos@gmail.com>
2021-05-07 18:06:39 -0300

    Adicionado requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -36,11 +36,35 @@ const getEmployeeByName = (employeeName) => {
   return employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 };
 
-console.log(getEmployeeByName());
+const personalInfo = {
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+};
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+const associatedWith = {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+  ]
+};
+
+const createEmployee = ({id, firstName, lastName}, {managers, responsibleFor}) => {
+  return {
+    'id': id,
+    'firstName': firstName,
+    'lastName': lastName,
+    'managers': managers,
+    'responsibleFor': responsibleFor
+  };
 }
+
+console.log(createEmployee(personalInfo, associatedWith));
 
 function isManager(id) {
   // seu código aqui
