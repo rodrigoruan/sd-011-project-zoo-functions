f156b020919a87e4fbe876ae830983c2c417aa92
josanjohnata <josanjohnata@gmail.com>
2021-05-07 18:25:07 -0300

    requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { species: animalSpecies, employees, prices } = require('./data');
+const { species: animalSpecies, employees, prices, hours } = require('./data');
 
 const getSpeciesByIds = (...ids) => ids
   .map((id) => animalSpecies.find((animal) => animal.id === id));
@@ -59,15 +59,15 @@ const getAnimalsByLocation = (result, animal) => {
   return result;
 };
 
-const residentsBySex = ( animal, sex) => animal
+const getResidentsBySex = (animal, sex) => animal
   .residents.reduce((residentsBySex, resident) => {
-    if (residents.sex === sex) residentsBySex.push(resident.name);
+    if (resident.sex === sex) residentsBySex.push(resident.name);
     return residentsBySex;
   }, []);
 
 const animalsByLocationNames = ([result, sorted, sex], animal) => {
   let residents = [];
-  if (sex) residents = residentsBySex(animal, sex);
+  if (sex) residents = getResidentsBySex(animal, sex);
   else residents = animal.residents.map((resident) => resident.name);
   if (sorted) residents.sort();
 
@@ -80,7 +80,7 @@ const animalsByLocationNames = ([result, sorted, sex], animal) => {
     }, sorted, sex];
   }
   result[animal.location].push(...[{
-    [animal.name]: resident,
+    [animal.name]: residents,
   }]);
 
   return [result, sorted, sex];
@@ -98,9 +98,24 @@ const getAnimalMap = (options) => {
   }
 };
 
-function getSchedule(dayName) {
-  // seu código aqui
-}
+const formatHour = (hour) => (hour <= 12 ? hour : hour - 12);
+
+const getScheduleByDay = (schedule, day) => Object.entries(schedule)
+  .reduce((filteredDay, [key, hour]) => {
+    if (key === day) return { ...filteredDay, [key]: hour };
+    return filteredDay;
+  }, {});
+
+const getSchedule = (dayName) => {
+  const formatedDays = Object.entries(hours).reduce((formated, [day, hour]) => ({
+    ...formated,
+    [day]: hour.open === 0 && hour.close === 0
+    ? 'CLOSED'
+    : `Open from &{formatHour(hour.open)}am unitl ${formatHour(hour.close)}pm`,
+  }), {});
+
+  return dayName ? getScheduleByDay(formatedDays, dayName) : formatedDays;
+};
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
