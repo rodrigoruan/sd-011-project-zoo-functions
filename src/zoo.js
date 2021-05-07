e6920a6fdf02b8acee05ebe0c0b7570ffb3cf2ff
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-05-07 15:24:33 -0300

    requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,10 +11,9 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  
-}
+const getSpeciesByIds = (...ids) => data.species.filter((species) => ids.some((id) => id === species.id));
 
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
 }
