211397622f35f9098de008a5b48278ee7d185a20
José Antônio Moura Neto <netomoura22@gmail.com>
2021-05-07 17:57:47 -0300

    cumprimento do requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -28,7 +28,8 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const newEmployee = {... personalInfo, ...associatedWith};
+  return newEmployee;
 }
 
 function isManager(id) {
