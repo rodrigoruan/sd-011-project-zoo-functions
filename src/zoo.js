2608c2e213caf1cc657cb9e43b19c7c67a19b21a
jhenriquemargrafm <jh.margraf@gmail.com>
2021-05-07 16:35:52 -0300

    Requisito 1 e 2: filter, find e every

--- src/zoo.js
+++ src/zoo.js
@@ -8,16 +8,21 @@ eslint no-unused-vars: [
   }
 ]
 */
-// Iniciando o projeto
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return data.species.filter(({ id }) => ids.includes((id)));
+  // Função Filter juntamente com includes para localizar todos os IDS passados nos parâmetros
 }
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(animal, minAge) {
+  const names = data.species.find(({ name }) => name === animal);
+  // função Find procura as ocorrências do primeiro parâmetro (a espécie de animal) na base de dados
+  return names.residents.every(({ age }) => age >= minAge);
+  // retorna true se todos os animais da espécie tem a idade maior que o parâmetro passado.
 }
+
+console.log(getAnimalsOlderThan('tigers', 12));
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
