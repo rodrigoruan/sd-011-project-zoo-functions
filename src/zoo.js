9b3a44d2d014aaf7d81e53ee5ed234f76ca0ce08
jhenriquemargrafm <jh.margraf@gmail.com>
2021-05-07 17:33:32 -0300

    Requisito 5:usando some

--- src/zoo.js
+++ src/zoo.js
@@ -45,7 +45,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some(({ managers }) => managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
