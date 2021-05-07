07ae4530de1b151c73ef4b6acc422736e6e6069a
PH Assis <pieit@live.com>
2021-05-07 15:42:59 -0300

    chlg 5 ready

--- src/zoo.js
+++ src/zoo.js
@@ -39,24 +39,15 @@ const createEmployee = (personalInfo, associatedWith) => {
   return genEmployee;
 };
 
-const personalInfo = {
-  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
-  firstName: 'John',
-  lastName: 'Doe',
-};
-
-const associatedWith = {
-  managers: ['c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1', '9e7d4524-363c-416a-8759-8aa7e50c0992'],
-  responsibleFor: [
-    '0938aa23-f153-4937-9f88-4858b24d6bce',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
-  ],
-};
-
-console.log(createEmployee(personalInfo, associatedWith));
-
 function isManager(id) {
+  const zooManagers = [
+    '9e7d4524-363c-416a-8759-8aa7e50c0992',
+    'fdb2543b-5662-46a7-badc-93d960fdc0a8',
+    '0e7b460e-acf4-4e17-bcb3-ee472265db83',
+  ];
+  const idFilter = data.employees.find((el) => el.id === id);
+  return zooManagers.some((el) => idFilter.id === el);
+
   // seu código aqui
 }
 
