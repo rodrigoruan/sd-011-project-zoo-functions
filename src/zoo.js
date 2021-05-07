eac2c259fe9c95e59a06f13e824ac11389c0bea8
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-05-07 17:14:05 -0300

    Realizada funçao getSpeciesByIds com MAp e Find.

--- src/zoo.js
+++ src/zoo.js
@@ -9,11 +9,19 @@ eslint no-unused-vars: [
 ]
 */
 
+// const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui play no game
+const species = data.species;
+const animals = data.animals;
+
+function getSpeciesByIds(...ids) {
+  return ids.map(id => {
+    return species.find(animal => animal.id === id);
+  });
+
 }
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
