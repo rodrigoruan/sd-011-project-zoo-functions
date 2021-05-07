b737e9473371f49c4f99e545dd79934a73aea946
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-07 15:01:11 -0300

    Porjeto Zoo Function Desafio 2

--- src/zoo.js
+++ src/zoo.js
@@ -19,6 +19,7 @@ function getSpeciesByIds(...ids) {
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return species.find((value) => value.name === animal).residents.every((value) => value.age >= age);
 }
 
 function getEmployeeByName(employeeName) {
