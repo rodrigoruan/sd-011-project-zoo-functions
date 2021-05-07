b7830a6221993d3dc0497f5d8c78a6fc65e4e5e5
Leandro Lopes <leandro_carlos@uol.com.br>
2021-05-07 18:55:25 -0300

    item 6

--- src/zoo.js
+++ src/zoo.js
@@ -52,8 +52,14 @@ function isManager(id) {
   return false;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function countAnimals(species) {
