12bba382160b9a006bc7ff422ab4d1495aab26a3
Alan Gonçalves <vekio.soft@gmail.com>
2021-05-07 17:15:38 -0300

    conclusão função getSpeciesByIds

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,12 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = require('./data');
+
+function getSpeciesByIds(...ids) {
+  return species.filter((value, index) => value.id === ids[index]);
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
