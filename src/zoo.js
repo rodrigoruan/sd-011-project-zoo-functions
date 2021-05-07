ff33349dc8a1b5a440912790e6a386a71b188688
PH Assis <pieit@live.com>
2021-05-07 14:35:07 -0300

    chlg 1 ready

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,15 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length > 0) {
+    const array = [];
+    ids.forEach((difId) => array.push(data.species.filter((el) => difId === el.id)));
+    return array.reduce((acc, curr) => acc.concat(curr));
+  }
+  return [];
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
