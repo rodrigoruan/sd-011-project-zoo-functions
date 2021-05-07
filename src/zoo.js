5e65ca45011dff25caf87f31bbb20ef3f9c2d5a5
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-05-07 17:18:55 -0300

    ex1 passed

--- src/zoo.js
+++ src/zoo.js
@@ -12,22 +12,19 @@ eslint no-unused-vars: [
 const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids, test = undefined) {
+function getSpeciesByIds(ids = [], ids2 = undefined) {
   if (ids !== undefined) {
     const speciesOfAnimals = species.filter((animals) => animals.id === ids);
-    if (test !== undefined) {
-      const speciesOfAnimals2 = species.filter((animals) => animals.id === test);
+    if (ids2 !== undefined) {
+      const speciesOfAnimals2 = species.filter((animals) => animals.id === ids2);
       const listOfSpecies = [...speciesOfAnimals, ...speciesOfAnimals2];
       console.log(listOfSpecies);
       return listOfSpecies;
     }
+    return speciesOfAnimals;
   }
   return ids;
 }
-const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
-const tigersId = 'e8481c1d-42ea-4610-8e11-1752cfc05a46';
-
-getSpeciesByIds(lionId, tigersId);
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
