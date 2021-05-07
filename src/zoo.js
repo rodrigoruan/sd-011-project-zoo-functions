c15e53a2f61c4eeb7bc396cae017eafa99902ed0
Laura Gusmao <laurargus@gmail.com>
2021-05-07 17:44:31 -0300

    Requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -17,10 +17,11 @@ function getSpeciesByIds(...ids) {
   // seu código aqui
   return species.filter((specie) => ids.some((id) => id === specie.id));
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return species.find((specie) => specie.name === animal).residents.every((resident) => resident.age > age);
 }
 
 function getEmployeeByName(employeeName) {
