4193b46180383d0a1fbf9552ecd0897a876bc913
Amandha W. Barok <amandha.barok@gmail.com>
2021-05-07 14:23:50 -0700

    Requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -66,8 +66,9 @@ function isManager(id) {
   return data.employees.some((value) => value.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = { id, firstName, lastName, managers, responsibleFor };
+  data.employees.push(newEmployee);
 }
 
 function countAnimals(species) {
