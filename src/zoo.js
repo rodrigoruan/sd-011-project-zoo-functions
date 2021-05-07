2c41d067339caa661963bdf47e7d4be281cd7408
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-05-07 19:11:32 -0300

    requisito 4 lint 1

--- src/zoo.js
+++ src/zoo.js
@@ -40,7 +40,7 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return {...personalInfo, ...associatedWith};
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
