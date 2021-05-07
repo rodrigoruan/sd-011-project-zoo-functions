fbc7458860aada32eeae0c0a7130b641ee8de31e
Rodolfo <rodolforrcont@gmail.com>
2021-05-07 17:17:29 -0300

    Requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -69,6 +69,21 @@ function getAnimalMap(options) {
 
 function getSchedule(dayName) {
   // seu código aqui
+  const result = {};
+  if (dayName && dayName !== 'Monday') {
+    result[dayName] = `Open from ${data.hours[dayName].open}am until ${data.hours[dayName].close - 12}pm`;
+    return result;
+  } if (dayName === 'Monday') {
+    return { Monday: 'CLOSED' };
+  }
+  return {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED' };
 }
 
 function getOldestFromFirstSpecies(id) {
