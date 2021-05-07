9c2d246a56a62341bd575a724a65a882c68b63ba
Gisele Costa <jikinha@gmail.com>
2021-05-07 15:40:50 -0300

    Linter

--- src/zoo.js
+++ src/zoo.js
@@ -12,51 +12,51 @@ eslint no-unused-vars: [
 const { species } = require('./data');
 const data = require('./data');
 
-const getSpeciesByIds = (...ids) => data.species.filter(species => ids.includes(species.id));
+const getSpeciesByIds = (...ids) => data.species.filter((specie) => ids.includes(specie.id));
 
-const getAnimalsOlderThan = (animal, age) => data.species.find((specie => specie.name === animal)).residents.every(animal => animal.age >= age);
+const getAnimalsOlderThan = (animal, age) => data.species.find(((specie) => specie.name === animal)).residents.every((anim) => anim.age >= age);
 
-const getEmployeeByName = (employeeName) => data.employees.find(({ firstName, lastName }) => (firstName ||lastName) === employeeName);
+const getEmployeeByName = (employeeName) => data.employees.find(({ firstName, lastName }) => (firstName || lastName) === employeeName);
 
 const createEmployee = (personalInfo, associatedWith) => {
   // seu código aqui
-}
+};
 
 const isManager = (id) => {
   // seu código aqui
-}
+};
 
 const addEmployee = (id, firstName, lastName, managers, responsibleFor) => {
   // seu código aqui
-}
+};
 
-const countAnimals = (species) => {
+const countAnimals = (specie) => {
   // seu código aqui
-}
+};
 
 const calculateEntry = (entrants) => {
   // seu código aqui
-}
+};
 
 const getAnimalMap = (options) => {
   // seu código aqui
-}
+};
 
 const getSchedule = (dayName) => {
   // seu código aqui
-}
+};
 
 const getOldestFromFirstSpecies = (id) => {
   // seu código aqui
-}
+};
 
 const increasePrices = (percentage) => {
   // seu código aqui
-}
+};
 
 const getEmployeeCoverage = (idOrName) => {
   // seu código aqui
-}
+};
 
 module.exports = {
   calculateEntry,
