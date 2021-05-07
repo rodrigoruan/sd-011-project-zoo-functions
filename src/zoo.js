c0b5954ee225ed70f66952dbf7e648a4bf45dacc
Tulio Sploradori <tuliospl@gmail.com>
2021-05-07 17:52:18 -0300

    conclusao do item 1

--- src/zoo.js
+++ src/zoo.js
@@ -9,17 +9,17 @@ eslint no-unused-vars: [
 ]
 */
 
+const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui !!
-  console.log('oi');
-  console.log('oi');
-  console.log('oi');
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {return []};
+  return species.filter((especie) => ids.includes(especie.id));  
 }
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+
+function getAnimalsOlderThan(animal, age) {  
 }
 
 function getEmployeeByName(employeeName) {
