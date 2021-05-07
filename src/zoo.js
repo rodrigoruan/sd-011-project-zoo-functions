241871a11f5cb41f2822025ce4cbf52d16a6bc96
Gisele Costa <jikinha@gmail.com>
2021-05-07 20:02:56 -0300

    getEmployeeCoverage

--- src/zoo.js
+++ src/zoo.js
@@ -9,38 +9,29 @@ eslint no-unused-vars: [
 ]
 */
 
-const { species, employees } = require('./data');
-const data = require('./data');
+const { species: animalSpecies, employees, prices, hours } = require('./data');
 
-const getSpeciesByIds = (...ids) => data.species.filter((value) => ids.includes(value.id));
+const getSpeciesByIds = (...ids) => animalSpecies.filter((value) => ids.includes(value.id));
 
-const getAnimalsOlderThan = (animal, age) => data.species.find(((value) => value.name === animal)).residents.every((value) => value.age >= age);
+const getAnimalsOlderThan = (animal, age) => animalSpecies.find(((value) => value.name === animal)).residents.every((value) => value.age >= age);
 
 const getEmployeeByName = (employeeName) => ((!employeeName) ? {}
-  : data.employees.find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName));
+  : employees.find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName));
 
 const createEmployee = (typealInfo, associatedWith) => ({ ...typealInfo, ...associatedWith });
 
-const isManager = (id) => data.employees.some(({ managers }) => managers.includes(id));
+const isManager = (id) => employees.some(({ managers }) => managers.includes(id));
 
-const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []) => data.employees.push({ id, firstName, lastName, managers, responsibleFor });
+const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []) => employees.push({ id, firstName, lastName, managers, responsibleFor });
 
-const countAnimals = (specie) => ((specie)
-  ? data.species.find(({ name }) => name === specie).residents.length
-  : data.species.reduce((acc, curr) => {
-    acc[curr.name] = curr.residents.length;
-    return acc;
-  }, {}));
+const countAnimals = (species) => ((species)
+  ? animalSpecies.find(({ name }) => name === species).residents.length
+  : animalSpecies.reduce((acc, { name, residents }) => ({ ...acc, [name]: residents.length }), {}));
 
 function calculateEntry(entrants) {
-  let total = 0;
+  if (!entrants) return 0;
 
-  if (entrants) {
-    const entrantKeys = Object.keys(entrants);
-    entrantKeys.forEach((value) => { total += data.prices[value] * entrants[value]; });
-  }
-
-  return total;
+  return Object.entries(entrants).reduce((fee, [type, qtt]) => fee + prices[type] * qtt, 0);
 }
 
 const getAnimalMap = (options) => {
@@ -55,7 +46,7 @@ const getString = (daySchedule) => {
 };
 
 const getSchedule = (dayName) => {
-  const schedule = data.hours;
+  const schedule = hours;
 
   if (dayName) return { [dayName]: schedule[dayName] };
 
@@ -67,15 +58,27 @@ const getSchedule = (dayName) => {
 };
 
 const getOldestFromFirstSpecies = (id) => {
-  const { age, name, sex } = data.species.find((specie) => specie.id === data.employees.find((employee) => employee.id === id).responsibleFor[0]).residents.reduce((acc, curr) => (curr.age > acc.age ? curr : acc), { age: 0 });
+  const { name, sex, age } = animalSpecies.find((specie) => specie.id === employees.find((employee) => employee.id === id).responsibleFor[0]).residents.reduce((acc, curr) => (curr.age > acc.age ? curr : acc), { age: 0 });
 
   return [name, sex, age];
 };
 
-const increasePrices = (percentage) => Object.keys(data.prices).forEach((priceKey) => { data.prices[priceKey] = Math.round(data.prices[priceKey] * (1 + percentage / 100) * 100) / 100; });
+// tive que usar Math.round, usando toFixed dá erro de 1 centavo
+const increasePrices = (percentage) => Object.keys(prices).forEach((priceKey) => { prices[priceKey] = Math.round(prices[priceKey] * (1 + percentage / 100) * 100) / 100; });
 
 const getEmployeeCoverage = (idOrName) => {
-  // seu código aqui
+  let filteredEmployees = employees;
+
+  if (idOrName) {
+    const hasName = getEmployeeByName(idOrName);
+    filteredEmployees = hasName ? [hasName] : [employees.find((employee) => employee.id === idOrName)];
+  }
+
+  const employeesWithSpecies = filteredEmployees.reduce((coverage, employee) => ({ ...coverage, [`${employee.firstName} ${employee.lastName}`]: employee.responsibleFor, }), {});
+
+  Object.entries(employeesWithSpecies).forEach(([key, animalsId]) => { employeesWithSpecies[key] = animalsId.map((animalId) => animalSpecies.find((specie) => specie.id === animalId).name); });
+
+  return employeesWithSpecies;
 };
 
 module.exports = {
