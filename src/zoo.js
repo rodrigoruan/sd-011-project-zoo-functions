746cfafc7546f996bfde81069788dbfa979ccecc
Luiz Fernando Rodrigues <nandovbr15@hotmail.com>
2021-05-07 15:16:08 -0300

    inicio do projeto

--- src/zoo.js
+++ src/zoo.js
@@ -13,6 +13,10 @@ const data = require('./data');
 
 function getSpeciesByIds(ids) {
   // seu código aqui
+  if (ids.length === 0) {
+    return [];
+  }
+  return data.species.filter((animal) => animal.id);
 }
 
 function getAnimalsOlderThan(animal, age) {
