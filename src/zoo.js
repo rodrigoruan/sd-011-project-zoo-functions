3d94480db4b812120a946c5ea2b524f9d9908dee
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-05-07 17:40:04 -0300

    Requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -28,7 +28,8 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const employee = { ...personalInfo, ...associatedWith };
+  return employee;
 }
 
 function isManager(id) {
