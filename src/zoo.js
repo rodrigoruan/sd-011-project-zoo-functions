68febe59f41a5d88b703cafc944e65382f0362ac
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-07 17:07:50 -0300

    initial commit

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  // initial commit
 }
 
 function getAnimalsOlderThan(animal, age) {
