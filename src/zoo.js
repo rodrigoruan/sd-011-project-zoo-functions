6db44f1c8742403887541d0428f04cc6dd4b5a83
marlon307 <marlon-ramosb@hotmail.com>
2021-05-07 18:14:20 -0300

    zoo-functions part 5

--- src/zoo.js
+++ src/zoo.js
@@ -15,26 +15,22 @@ const {
 } = require('./data');
 
 function getSpeciesByIds(...ids) {
-  // seu código aqui
   return species.filter((filterid, index) => filterid.id === ids[index]);
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
   const anm = species.find((animall) => animall.name === animal);
   const result = anm.residents.every((elem) => elem.age > age);
   return result;
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
   let result = employees.filter((name) => name.firstName === employeeName || name.lastName === employeeName)[0];
   if (result === undefined) return {};
   return result;
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
   return {
     ...personalInfo,
     ...associatedWith,
@@ -42,8 +38,10 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some(() => id === '0e7b460e-acf4-4e17-bcb3-ee472265db83');
 }
+
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
