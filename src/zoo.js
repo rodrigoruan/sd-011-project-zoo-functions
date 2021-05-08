85d6605834408a0dbb29e30709c069265bc7fd99
IgorMendez <igor.emp@hotmail.com>
2021-05-08 18:19:27 -0300

    Ex2

--- src/zoo.js
+++ src/zoo.js
@@ -10,13 +10,17 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const { species } = require('./data');
 
-function getSpeciesByIds(ids) {
-  // a
+function getSpeciesByIds(...ids) {
+  const idsAnimals = species.filter((el) => ids.some((id) => id === el.id));
+  return idsAnimals;
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species
+    .find((el) => el.name.includes(animal))
+    .residents.every((resident) => resident.age > age);
 }
 
 function getEmployeeByName(employeeName) {
@@ -35,7 +39,7 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
 
-function countAnimals(species) {
+function countAnimals(species1) {
   // seu código aqui
 }
 
