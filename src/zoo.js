370e4ca00b3d294755b10a96fd702e3ac75e785e
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-05-07 21:11:01 -0300

    Requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -41,8 +41,16 @@ function isManager(id) {
   return data.employees.some((person) => person.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push(
+    {
+      id,
+      firstName,
+      lastName,
+      managers,
+      responsibleFor,
+    },
+  );
 }
 
 function countAnimals(species) {
