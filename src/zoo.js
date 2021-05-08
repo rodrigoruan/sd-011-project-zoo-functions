6665ceadacb8a9ba90c071b2d00630843f8fd1c4
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-05-08 16:07:50 -0300

    Requisito 4 concluido

--- src/zoo.js
+++ src/zoo.js
@@ -32,7 +32,7 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return Object.assign(personalInfo, associatedWith);
 }
 
 function isManager(id) {
