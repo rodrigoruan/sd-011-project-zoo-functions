55769d5ffcdf17ec1e396cbb597c5c877e30bd70
Gustavo Mendes <gusm217@gmail.com>
2021-05-08 18:57:25 -0300

    Adiciona o 4

--- src/zoo.js
+++ src/zoo.js
@@ -12,51 +12,38 @@ eslint no-unused-vars: [
 const { species, employees } = require("./data");
 const data = require("./data");
 
-// function getSpeciesByIds(...ids) {
-//   // seu código aqui
-//   if (!ids) return [];
-//   return ids.map((id) => species.find((animal) => id === animal.id));
-// }
+function getSpeciesByIds(...ids) {
+  // seu código aqui
+  if (!ids) return [];
+  return ids.map((id) => species.find((animal) => id === animal.id));
+}
 
-// console.log(getSpeciesByIds());
+console.log(getSpeciesByIds());
 
-// function getAnimalsOlderThan(animal, age) {
-//   // seu código aqu
-//   const animalName = species.find((specie) => specie.name === animal);
-//   return animalName.residents.every((animal) => animal.age >= age);
-// }
-// // Filter retornava o residents como objetcs, por isso não acessava. Com find ele acessa todas as infos.
-// console.log(getAnimalsOlderThan("tigers", 20));
+function getAnimalsOlderThan(animal, age) {
+  // seu código aqu
+  const animalName = species.find((specie) => specie.name === animal);
+  return animalName.residents.every((animal) => animal.age >= age);
+}
+// Filter retornava o residents como objetcs, por isso não acessava. Com find ele acessa todas as infos.
+console.log(getAnimalsOlderThan("tigers", 20));
 
-// function getEmployeeByName(employeeName) {
-//   // seu código aqui
-//   if (!employeeName) return {};
-//   return employees.find(
-//     (name) => name.firstName === employeeName || name.lastName === employeeName
-//   );
-// }
+function getEmployeeByName(employeeName) {
+  // seu código aqui
+  if (!employeeName) return {};
+  return employees.find(
+    (name) => name.firstName === employeeName || name.lastName === employeeName
+  );
+}
 
-// console.log(getEmployeeByName());
+console.log(getEmployeeByName());
 
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-//   const newEmployInfo = { personalInfo };
-//   const newEmployAssoc = associatedWith;
-//   const { id, firstName, lastName } = newEmployInfo;
-//   const { managers, responsibleFor } = newEmployAssoc;
-
-//   // const { id, firstName, lastName } = personalInfo;
-//   // const { managers, responsibleFor } = associatedWith;
-//   // const newEmploy = {
-//   //   id: personalInfo.id,
-//   //   firstName: personalInfo.firstName,
-//   //   lastName: personalInfo.lastName,
-//   //   managers: associatedWith.managers,
-//   //   responsibleFor: associatedWith.responsibleFor,
-//   // };
-
-//   return { id, firstName, lastName, managers, responsibleFor };
-// }
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return { id, firstName, lastName, managers, responsibleFor };
+}
 
 function isManager(id) {
   // seu código aqui
