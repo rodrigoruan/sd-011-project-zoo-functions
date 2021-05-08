fd51f1061a7ee0b9206bf12dc7dfabed2e618f33
gab-boff <gmeninboff@gmail.com>
2021-05-08 13:58:40 -0300

    Adicionando Requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -29,11 +29,16 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  return data.employees.some((employee) => employee.managers.includes(id));
+  if (id === '9e7d4524-363c-416a-8759-8aa7e50c0992'
+    || id === 'fdb2543b-5662-46a7-badc-93d960fdc0a8'
+    || id === '0e7b460e-acf4-4e17-bcb3-ee472265db83') {
+    return true;
+  }
+  return false;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function countAnimals(species) {
