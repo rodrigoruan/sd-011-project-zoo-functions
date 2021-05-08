4d04f8a486b5475fbd35a7e3ddceb8a2de760cc9
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-05-07 21:46:16 -0300

    Implementa a funcao isManager

--- src/zoo.js
+++ src/zoo.js
@@ -46,8 +46,11 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  const manage = data.employees.some((employee) => employee.managers.includes(id));
+  return manage;
 }
+
+console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
