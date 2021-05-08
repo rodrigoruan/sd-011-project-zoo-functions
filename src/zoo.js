09a6217c77dc3797745ea56f41db146799cc3bf9
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-05-07 21:00:28 -0300

    ex5 passed

--- src/zoo.js
+++ src/zoo.js
@@ -43,8 +43,12 @@ function createEmployee(personalInfo, associatedWith) {
   return createEmployeeDescription;
 }
 
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
 function isManager(id) {
-  // seu código aqui
+  const findIdManage = employees.filter((employeer) => employeer.id === id).every((identidy) => identidy.id === stephanieId || identidy.id === olaId || identidy.id === burlId);
+  return findIdManage;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
