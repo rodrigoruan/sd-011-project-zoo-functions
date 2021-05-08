b0a8867228e2cbc8d0ec74c601a68403008c0e56
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-08 12:15:15 -0300

    Refatorando as funcoes e comentando

--- src/zoo.js
+++ src/zoo.js
@@ -10,41 +10,61 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-
+//  Requerimentos do arquivo data para facilitar a leitura.
 const { species } = data;
 const { employees } = data;
 
-const getSpeciesByIds = (...ids) => (ids ? species.filter((animal) => [...ids].includes(animal.id)) : []);
+//  Se o ids for diferente de 0 = negativo, retornara todas as especies que estão no ids, caso não haja, retorna um obj vazio.
+const getSpeciesByIds = (...ids) =>
+  (ids ? species.filter((animal) => [...ids].includes(animal.id)) : []);
 
-const getAnimalsOlderThan = (animal, age) => species.find((animalName) => animalName.name === animal).residents.every((animals) => animals.age >= age);
+//  Encontra nas species o nome do animal e faz uma varredura, se todas idades dos animais forem >= age, retorna true, otherwise retorna falso.
+const getAnimalsOlderThan = (animal, age) =>
+  species.find((animalName) => animalName.name === animal).residents.every((animals) => animals.age >= age);
 
-const getEmployeeByName = (employee) => (employee ? employees.find((name) => name.firstName === employee || name.lastName === employee) : {});
+//  Se employee for diferente de 0 = negativo, encontra o funcionario e o retorna, otherwise retorna um empty object.
+const getEmployeeByName = (employee) =>
+  (employee ? employees.find((name) => name.firstName === employee || name.lastName === employee) : {});
 
+//  Cria um novo funcionario com as informações passadas em personalInfo e associatedWith, obs: os dois param serão um obj, por isso o uso do spread operator.
 const createEmployee = (personalInfo, associatedWith) => ({ ...personalInfo, ...associatedWith });
 
+//  verifica nos employess se algum contém o id passado em managers, caso yes retorn true, otherwise false.
 const isManager = (id) => employees.some((worker) => worker.managers.includes(id));
 
-const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []) => employees.push({ id,
-  firstName,
-  lastName,
-  managers,
-  responsibleFor,
-});
+//  adicionado um employee ao no data.js, caso managers e responsibleFor não forem passados, retorna um empty array.
+const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []) =>
+  employees.push({ id, firstName, lastName, managers, responsibleFor });
 
-const countAnimals = (specie) => (specie ? species.find((animal) => animal.name === specie).residents.length : Object.fromEntries(species.map((animal) => [animal.name, animal.residents.length])));
+const countAnimals = (specie) =>
+  (specie ? species.find((animal) => animal.name === specie).residents.length
+    : Object.fromEntries(species.map((animal) => [animal.name, animal.residents.length])));
 
-const calculateEntry = (entrants = 0) => (Object.entries(entrants).length ? Object.entries(entrants).reduce((acc, curr) => acc + data.prices[curr[0]] * curr[1], 0) : 0);
+const calculateEntry = (entrants = 0) =>
+  (Object.entries(entrants).length
+    ? Object.entries(entrants).reduce((acc, curr) => acc + data.prices[curr[0]] * curr[1], 0) : 0);
 
 const animalsName = (...args) => {
-  const nameOfAnimals = args[0].reduce((acc, curr) => (args[2] && curr.sex !== args[2] ? acc : acc.concat(curr.name)), []); return args[1] ? nameOfAnimals.sort() : nameOfAnimals;
+  const nameOfAnimals = args[0].reduce((acc, curr) => (args[2] && curr.sex !== args[2] ? acc : acc.concat(curr.name)), []);
+  return args[1] ? nameOfAnimals.sort() : nameOfAnimals;
 };
 
 const getAnimalMap = (options = {}) => {
-  let obj = {}; const names = ['NE', 'NW', 'SE', 'SW']; for (let i of names) {
+  let obj = {};
+  const names = ['NE', 'NW', 'SE', 'SW'];
+
+  for (let i of names) {
     obj[i] = [];
-  } species.map((animal) => (options.includeNames ? obj[animal.location].push({ [animal.name]: animalsName(animal.residents, options.sorted, options.sex) }) : obj[animal.location].push(animal.name))); return obj;
+  }
+
+  species.map((animal) =>
+    (options.includeNames ? obj[animal.location].push({ [animal.name]:
+       animalsName(animal.residents, options.sorted, options.sex),
+    }) : obj[animal.location].push(animal.name)));
+  return obj;
 };
 
+//  Objeto com todos os dias da semana do zoológico.
 const daysOfWeek = {
   Tuesday: 'Open from 8am until 6pm',
   Wednesday: 'Open from 8am until 6pm',
@@ -55,28 +75,49 @@ const daysOfWeek = {
   Monday: 'CLOSED',
 };
 
+//  Caso passado algum parâmetro para day, retornará o dia e o horário aberto apenas no dia, otherwise retornará o objeto daysOfWeek com todos os dias da semana e seus horários.
 const getSchedule = (day) => (day ? Object.fromEntries([[day, daysOfWeek[day]]]) : daysOfWeek);
 
-const getOldestFromFirstSpecies = (id) => Object.values(species.find((animal) => animal.id === employees.find((person) => person.id === id).responsibleFor[0]).residents.sort((animal1, animal2) => animal2.age - animal1.age)[0]);
+const getOldestFromFirstSpecies = (id) =>
+  Object.values(species.find((animal) => animal.id === employees.find((person) => person.id === id).responsibleFor[0])
+    .residents.sort((animal1, animal2) => animal2.age - animal1.age)[0]);
 
+//  Função para calcular a porcentagem que será adicionado aos preços.
 const addPercentage = (peop, percentage) => Math.round((peop + peop * (percentage / 100)) * 100) / 100;
 
+//  Função que aumenta os preços por porcentagem no data.js: Adult, Child e Senior.
 const increasePrices = (percent) => {
-  const { prices } = data; const { Adult, Child, Senior } = prices; prices.Adult = addPercentage(Adult, percent); prices.Child = addPercentage(Child, percent); prices.Senior = addPercentage(Senior, percent);
+  const { prices } = data;
+  const { Adult, Child, Senior } = prices;
+  prices.Adult = addPercentage(Adult, percent);
+  prices.Child = addPercentage(Child, percent);
+  prices.Senior = addPercentage(Senior, percent);
 };
 
+//  Função que encontra o funcionário por id name ou lastname, os animais pelo qual o funcionário é responsável e retorna um objeto com o nome completo e um array com os animais pelo qual o mesmo é responsável.
 const employeeWithId = (idOrName) => {
   const person = employees.find((people) => people.id === idOrName || people.firstName === idOrName || people.lastName === idOrName);
-  const personFullName = `${person.firstName} ${person.lastName}`; const animalName = person.responsibleFor.map((id) => species.find((animal) => animal.id === id)).map((animal) => animal.name);
-  let obj = {}; obj[personFullName] = animalName; return obj;
+
+  const personFullName = `${person.firstName} ${person.lastName}`;
+  const animalName = person.responsibleFor.map((id) => species.find((animal) => animal.id === id)).map((animal) => animal.name);
+
+  let obj = {};
+  obj[personFullName] = animalName;
+
+  return obj;
 };
 
+//  Função que retorna todos os funcionários com todos os animais pelo qual são responsáveis, reutilizado a mesma função para encontrar o funcionário por id.
 const employeeWithoutId = () => {
-  let arrOfEmployees = employees.map((person) => person.firstName); let obj = {}; for (let i of arrOfEmployees) {
+  let arrOfEmployees = employees.map((person) => person.firstName);
+  let obj = {};
+  for (let i of arrOfEmployees) {
     Object.assign(obj, employeeWithId(i));
-  } return obj;
+  }
+  return obj;
 };
 
+//  Função final onde serão ultilizada as duas últimas funções criadas, caso seja passado um id retornará o funcionário e os animais pelo qual o mesmo é resposável, otherwise retornará todos os funcionários com os animais pelo qual são responsáveis.
 const getEmployeeCoverage = (idOrName) => (idOrName ? employeeWithId(idOrName) : employeeWithoutId());
 
 module.exports = {
