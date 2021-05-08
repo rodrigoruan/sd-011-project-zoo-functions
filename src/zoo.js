0f45900943008023eebfc330a564984969e08c77
renan <renan_menezes07@hotmail.com>
2021-05-08 15:23:40 -0300

    Resolução do exercício 11.

--- src/zoo.js
+++ src/zoo.js
@@ -47,29 +47,65 @@ function isManager(id) {
   return data.employees.some((employee) => employee.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee({ id, firstName, lastName, managers, responsibleFor }) {
+  // Adiciona uma nova pessoa colaboradora ao array employees, presente no arquivo data.js.
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function countAnimals(species) {
-  // seu código aqui
+  // Contabiliza a quantidade de animais.
+  // data.species.find((specie) => specie.resident.length,s
+  // data.species.map((specie) => ({ `${specie.name}`: specie.residents.length })));
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  // A partir da quantidade de visitantes e a faixa etária de cada um, esta função é responsável por retornar o preço total a ser cobrado.
+  // if (!entrants || entrants === {}) return 0;
+  // const prices = {adult: 49.99, child: 20.99, senior: 24.99};
+  // entrants.array.forEach(element => {
+  
+  // return totalPaid;
 }
 
-function getAnimalMap(options) {
-  // seu código aqui
+function getAnimalMap({ includeNames, sorted, sex }) {
+  // const filterByRegion = (local) => data.species.filter((specie) => specie.location === local).reduce((animalAcc, specie) => animalAcc.push(specie.name));
+  if (includeNames) {
+    const getResidentListByRegion = (local) => {
+      data.species
+        .filter((specie) => specie.location === local)
+        .forEach((specie) => specie
+          .map(() => {
+            specie.name = [];
+            specie.name.push(specie.residents);
+            if (sorted) return specie.name.sort(); return specie.name;
+          }));
+    };
+    residentListNE = { ...getResidentListByRegion('NE') };
+    residentListNW = { ...getResidentListByRegion('NW') };
+    residentListSE = { ...getResidentListByRegion('SE') };
+    residentListSW = { ...getResidentListByRegion('SW') };
+
+    return data.species.map(() => ({ NE: [...getResidentListByRegion('NE')], NW: [...getResidentListByRegion('NW')], SE: [...getResidentListByRegion('SE')], SW: [...getResidentListByRegion('SW')] }));
+  }
+  // data.species.map(() => ({ NE: filterByRegion('NE'), NW: filterByRegion('NW'), SE: filterByRegion('SE'), SW: filterByRegion('SW') }));
 }
+// console.log(data.species.filter((specie) => specie.location === 'NE'));
 
 function getSchedule(dayName) {
-  // seu código aqui
+  // if (!dayName) {
+  // const weekDaysAndHours = Object.entries(data.hours);
+  // return weekDaysAndHours.map((day) => {{ ola: `Open from ${day[1].open}am until ${day[1].close}pm`, }});
+  // }
 }
+// console.log(getSchedule());
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getAnimalIdbyEmployee = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const oldestAnimalAge = data.species.find((specie) => specie.id === getAnimalIdbyEmployee).residents.reduce((acc, resident) => acc < resident.age ? acc = resident.age : acc, 0);
+  const oldestResident = data.species.find((specie) => specie.id === getAnimalIdbyEmployee).residents.filter((resident) => resident.age === oldestAnimalAge);
+  return Object.values(oldestResident[0]);
 }
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 
 function increasePrices(percentage) {
   // seu código aqui
