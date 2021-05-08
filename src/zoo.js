abad2f8db7d26f84f7175af8fafce907ab7c5d58
Marco Galindo <marco1408@gmail.com>
2021-05-08 14:10:59 -0300

    exercício 10 finalizado

--- src/zoo.js
+++ src/zoo.js
@@ -71,8 +71,23 @@ function getAnimalMap(options) {
 }
 
 function getSchedule(dayName) {
-  // seu código aqui
+  const { Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday } = data.hours;
+  const schedule = {
+    Tuesday: `Open from ${Tuesday.open}am until ${Tuesday.close - 12}pm`,
+    Wednesday: `Open from ${Wednesday.open}am until ${Wednesday.close - 12}pm`,
+    Thursday: `Open from ${Thursday.open}am until ${Thursday.close - 12}pm`,
+    Friday: `Open from ${Friday.open}am until ${Friday.close - 12}pm`,
+    Saturday: `Open from ${Saturday.open}am until ${Saturday.close - 12}pm`,
+    Sunday: `Open from ${Sunday.open}am until ${Sunday.close - 12}pm`,
+    Monday: 'CLOSED',
+  };
+  if (!dayName) return schedule;
+  return {
+    [dayName]: schedule[dayName],
+  };
 }
+
+getSchedule('Tuesday');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
