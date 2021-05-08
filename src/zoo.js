280a038eabe887b97be6d6cc23f65fbaf3bb7384
DADA <davimarque2000@gmail.com>
2021-05-08 10:34:02 -0300

    requisito 7, countAnimals

--- src/zoo.js
+++ src/zoo.js
@@ -26,7 +26,6 @@ function getEmployeeByName(employeeName) {
   return employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
 }
 
 function isManager(id) {
@@ -45,10 +44,20 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   return data.employees.push(newEmploye);
 }
 
-function countAnimals() {
-  // seu código aqui
+function countAnimals(especies) {
+  if (especies !== undefined) { return species.find((specie) => specie.name === especies).residents.length; }
+  return {
+    lions: species.find((specie) => specie.name === 'lions').residents.length,
+    tigers: species.find((specie) => specie.name === 'tigers').residents.length,
+    bears: species.find((specie) => specie.name === 'bears').residents.length,
+    penguins: species.find((specie) => specie.name === 'penguins').residents.length,
+    otters: species.find((specie) => specie.name === 'otters').residents.length,
+    frogs: species.find((specie) => specie.name === 'frogs').residents.length,
+    snakes: species.find((specie) => specie.name === 'snakes').residents.length,
+    elephants: species.find((specie) => specie.name === 'elephants').residents.length,
+    giraffes: species.find((specie) => specie.name === 'giraffes').residents.length,
+  };
 }
-
 function calculateEntry(entrants) {
   // seu código aqui
 }
