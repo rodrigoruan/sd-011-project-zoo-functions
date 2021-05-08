8361461461792b143a53d89e38348f637a665367
André Dantas <aluizcfd@gmail.com>
2021-05-08 19:28:32 -0300

    Added base value to managers and responsibleFor, requirement 6

--- src/zoo.js
+++ src/zoo.js
@@ -34,7 +34,7 @@ function isManager(id) {
   return employees.some(({ managers }) => managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   employees.push({
     id,
     firstName,
