bf60d8ee69ed39e11a78cb6d7a95a5c2e51eb736
Jessica Arruda <sicarruda@gmail.com>
2021-05-09 18:55:00 -0300

    exercicio isManager

--- src/zoo.js
+++ src/zoo.js
@@ -43,10 +43,14 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // if (id === stephanieId || id === olaId || id === urlId) {
-  //   return true;
-  // }
-  // return false;
+  if (
+    id === '9e7d4524-363c-416a-8759-8aa7e50c0992'
+    || id === 'fdb2543b-5662-46a7-badc-93d960fdc0a8'
+    || id === '0e7b460e-acf4-4e17-bcb3-ee472265db83'
+  ) {
+    return true;
+  }
+  return false;
   // seu código aqui
 }
 
