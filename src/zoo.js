b34d5d376ff952950b7cce97ffc927096d588715
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-09 12:53:03 -0300

    task 2 test 1.0

--- src/zoo.js
+++ src/zoo.js
@@ -15,14 +15,19 @@ const data = require('./data');
 // JS filter: https://www.w3schools.com/jsref/jsref_filter.asp
 // JS includes: https://www.w3schools.com/jsref/jsref_includes.asp
 // JS rest parameter: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parametersb
+// JS arrow function: https://www.w3schools.com/js/js_arrow_function.asp
+// Working with Objects: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
 
 function getSpeciesByIds(...id) {
-  // element = variable element
-  return data.species.filter((element) => id.includes(element.id));
+  return data.species.filter((items) => id.includes(items.id));
 }
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+// References:
+// JS find: https://www.w3schools.com/jsref/jsref_find.asp
+// JS every: https://www.w3schools.com/jsref/jsref_every.asp
+
+function getAnimalsOlderThan(animals, age) {
+  return data.species.find((items) => animals.includes(items.name)).residents.every((items) => (items.age >= age));
 }
 
 function getEmployeeByName(employeeName) {
