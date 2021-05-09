5793bfdae7a71efc1015a6ce573fa2a6fbc59813
renan <renan_menezes07@hotmail.com>
2021-05-09 10:35:36 -0300

    Resolução do requisito 2.

--- src/zoo.js
+++ src/zoo.js
@@ -20,19 +20,25 @@ function getSpeciesByIds(...idCode) {
   return specieList;
 }
 
-function getAnimalsOlderThan(animal, age) {
+function getAnimalsOlderThan(animal, age) { // Tá dizendo que residents is undefined.
   // A partir do nome de uma espécie e uma idade mínima, 
   // verifica se todos os animais daquela espécie possuem a idade mínima especificada.
-  return data.species.find((specie) => specie.name === animal).every((resident) => resident.age >= age);
+  // return data.species.find((specie) => specie.name === animal).every((resident) => resident.age >= age);
+  const isOldEnough = data.species.filter((specie) => specie.name === animal)[0].residents.every((resident) => resident.age >= age);
+  return isOldEnough;
 }
+console.log(getAnimalsOlderThan('otters', 7));
 
-function getEmployeeByName(employeeName) {
+function getEmployeeByName(employeeName) { // Tá imprimindo array ao invés de object.
   // Busca das pessoas colaboradoras através do primeiro ou do último nome delas.
   if (!employeeName) {
     return {};
   }
-  return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  const employeeData = data.employees.filter((employee) => employeeName === employee.name || employeeName === employee.lastName);
+  return employeeData;
+  // return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
+// console.log(getEmployeeByName('Wishart'));
 
 function createEmployee(personalInfo, associatedWith) {
   // const ({ id, firstName, lastName } = personalInfo);
