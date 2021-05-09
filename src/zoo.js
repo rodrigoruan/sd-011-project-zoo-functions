ea1763218d7aa8f81efdd37c95b591bc912d228f
Genivaldo <cgenivaldo46@gmail.com>
2021-05-09 09:49:50 -0300

    Requisito-01.9

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ const data = require('./data');
 
 function getSpeciesByIds(ids) {
   // seu código
-  const especies = ((species) => data.species.id);
+  const especies = ((id) => (data.species.id));
   return especies;
 }
 function getAnimalsOlderThan(animal, age) {
