0c27045312b4132d60b3af7ef5bc39fde2fb7918
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-05-09 11:15:55 -0300

    Requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -34,7 +34,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((item) => item.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
