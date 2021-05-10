a4fa6addd3e127b7ec585087c67f3be714c2bc3f
BeatrizR <biarl.ramalhorl@gmail.com>
2021-05-10 18:25:45 -0300

    teste

--- src/zoo.js
+++ src/zoo.js
@@ -8,7 +8,7 @@ eslint no-unused-vars: [
   }
 ]
 */
-const { employees, prices, species } = require('./data');
+//const { employees, prices, species } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
