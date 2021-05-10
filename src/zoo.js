4fdc3b454f0e4b54fd4479231cd180399a9dad52
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-05-10 15:53:19 -0300

    Requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -19,7 +19,9 @@ function getAnimalsOlderThan(animal, age) {
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+
+  return data.employees.find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
