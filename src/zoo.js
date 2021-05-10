0c0eed2214f220d11d6ea7f71f4bfb1b0d1a233c
heyynat <natalifplima@gmail.com>
2021-05-10 14:40:10 -0400

    Requisito 7 1/2

--- src/zoo.js
+++ src/zoo.js
@@ -26,27 +26,25 @@ function getEmployeeByName(employeeName) {
     return {};
   }
   return data.employees
-  .find((person) => employeeName.includes(person.firstName) || employeeName.includes(person.lastName));
+    .find((person) => employeeName.includes(person.firstName) || employeeName.includes(person.lastName));
 }
 
-
-
 function createEmployee(personalInfo, associatedWith) {
-  const {id, firstName, lastName} = personalInfo
-  const {managers, responsibleFor} = associatedWith
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
   return {
     id,
     firstName,
     lastName,
     managers,
-    responsibleFor
-  }
+    responsibleFor,
+  };
 }
 
 function isManager(id) {
   // Recebi ajuda de Laura Gusmão e Mikaela Braga
   return data.employees
-  .some((person) => person.managers.includes(id));
+    .some((person) => person.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
@@ -55,16 +53,25 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
     firstName,
     lastName,
     managers,
-    responsibleFor
+    responsibleFor,
   };
   data.employees.push(newPerson);
   return newPerson;
 }
 
 function countAnimals(species) {
-  // seu código aqui
+  if (species === undefined) {
+    return data.species.reduce((acc, animals) => {
+      acc[animals.name] = animals.residents.length;
+      return acc;
+    }, {});
+  }
+  // return data.species.reduce((acc, animals)=> {
+  //   acc = animals.residents.length;
+  //   return acc
+  // });
 }
-
+// console.log(countAnimals('snakes'));
 function calculateEntry(entrants) {
   // seu código aqui
 }
