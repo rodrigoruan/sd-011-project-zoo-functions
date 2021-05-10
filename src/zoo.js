9dae199e8a3589ed5f2dcc2020c44f0be56c2487
WalterCosta <156-201707140059@users.noreply.gitlab.lasse.ufpa.br>
2021-05-10 14:58:32 -0300

    Feito item 1

--- src/zoo.js
+++ src/zoo.js
@@ -1,5 +1,5 @@
 /*
-eslint no-unused-vars: [ 
+eslint no-unused-vars: [
   "error",
   {
     "args": "none",
@@ -8,12 +8,44 @@ eslint no-unused-vars: [
   }
 ]
 */
-
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
+/* console.log(data.species[0].id);
+console.log(data.species[1].id);
+console.log(data.species[2].id);
+console.log(data.species[3].id);
+console.log(data.species[4].id);
+console.log(data.species[5].id);
+console.log(data.species[6].id);
+console.log(data.species[7].id);
+console.log(data.species[8].id);
+ */
+/*
+  Para imprimir todos os IDs
+  for(let index= 0; index < data.species.length; index += 1){
+  console.log(data.species[index].id);
+} */
+
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  // Para caso nenhum parametro seja informado, retorna um array vazio
+  if (ids.length === 0) {
+    return [];
+  }
+
+  const filterID = data.species.filter((value) => {
+    let moreOneId = false;
+    for (let index = 0; index < ids.length; index += 1) {
+      if (value.id === ids[index]) {
+        moreOneId = true;
+      }
+    }
+    return moreOneId;
+  });
+  return filterID;
 }
+
+// console.log(getSpeciesByIds(data));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
