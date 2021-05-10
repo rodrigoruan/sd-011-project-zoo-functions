33dc65b82fa249f2958566545449e36278755f1f
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-05-10 19:40:51 -0300

    Requisito 4 correçoes lint

--- src/zoo.js
+++ src/zoo.js
@@ -49,7 +49,7 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return {...personalInfo, ...associatedWith};
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
