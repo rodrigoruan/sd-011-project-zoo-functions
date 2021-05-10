cbc4f0a3ed0117858e21ea29bcd060791eb10ac6
Savio Moraes <smmsavio@hotmail.com>
2021-05-10 00:19:53 -0300

    requisito 02

--- src/zoo.js
+++ src/zoo.js
@@ -9,17 +9,26 @@ eslint no-unused-vars: [
 ]
 */
 
-// const { ...id } = require('./data');
 const data = require('./data');
+// console.log(species.length);
+const { species: animalNames } = require('./data');
+// console.log(...animalNames);
+const { employees: employeesId } = require('./data');
+// console.log(...employeesId);
+const { hours } = require('./data');
+// console.log(hours);
+const { prices } = require('./data');
+// console.log(prices);
 
-function getSpeciesByIds(...ids) {
-  if (ids.length === 0) return [];
-  return data.species.filter(({ id }) => ids.find((animalsId) => animalsId === id));
-}
+// Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
+// Caso receba nenhum parâmetro, necessário retornar um array vazio
+// Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id
+// Ao receber mais de um id, retorna um array com as espécies referentes aos ids
+const getSpeciesByIds = (...ids) => data.species.filter(({ id }) => ids.find((animalId) => animalId === id));
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+// Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada (>=)
+// retorna um valor booleano
+const getAnimalsOlderThan = (animal, age) => animalNames.find((animals) => animals.name === animal).residents.every((resident) => resident.age >= age);
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
