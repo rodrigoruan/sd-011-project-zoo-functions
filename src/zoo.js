8bda1988da10bd2282fb3b3ad01f4f48ab2ae6f0
Gabriel <gribeiro237@gmail.com>
2021-05-10 17:29:47 -0300

    requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,8 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return data.species.filter((Species) => ids.some((id) => Species.id === id));
 }
 
 function getAnimalsOlderThan(animal, age) {
