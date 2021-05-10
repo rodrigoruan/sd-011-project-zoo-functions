c0d607cba8b15909108dc64e7862b468ae159357
Juliana Happatsch <jhappatsch@gmail.com>
2021-05-10 16:58:32 -0300

    requisito 8

--- src/zoo.js
+++ src/zoo.js
@@ -9,23 +9,24 @@ eslint no-unused-vars: [
 ]
 */
 
-const { species, employees } = require('./data');
+const { species, employees, prices } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
-  return species.filter((specie) => ids.includes(specie.id));
+  return species.filter((animals) => animals.id.includes(ids));
 }
 
 function getAnimalsOlderThan(animal, age) {
   const findingName = (specie) => specie.name === animal;
-  const comparingAge = (resident) => resident.age >= age;
+  const checkingAge = (resident) => resident.age >= age;
 
-  return species.find(findingName).residents.every((comparingAge));
+  return species.find(findingName).residents.every(checkingAge);
 }
 
 function getEmployeeByName(employeeName) {
   if (!employeeName) return {};
-  return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+
+  return employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -33,26 +34,34 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  return data.employees.some(({ managers }) => managers.includes(id));
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  return data.employees.push({ id, firstName, lastName, managers, responsibleFor });
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function countAnimals(specie) {
   if (!specie) {
     const animalsObj = {};
-    species.forEach((animals) => { animalsObj[animals.name] = animals.residents.length; });
+    species.forEach((animal) => {
+      const { name, residents } = animal;
+      animalsObj[name] = residents.length;
+    });
 
     return animalsObj;
   }
 
-  return species.find((animals) => animals.name === specie).residents.length;
+  return species.find((animal) => animal.name === specie).residents.length;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+
+  const { Adult = 0, Child = 0, Senior = 0 } = entrants;
+  return (Adult * prices.Adult) + (Child * prices.Child) + (Senior * prices.Senior);
 }
 
 function getAnimalMap(options) {
