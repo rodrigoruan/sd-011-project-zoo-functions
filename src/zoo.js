1a607a86dcd3b9155833bac179f38161096d6c7b
Guilherme Nunes <guiicnunes@gmail.com>
2021-05-10 15:03:55 -0300

    Req 6

--- src/zoo.js
+++ src/zoo.js
@@ -24,9 +24,9 @@ const createEmployee = (personalInfo, associatedWith) => ({ ...personalInfo, ...
 
 const isManager = (id) => data.employees.reduce(((acc, { managers }) => (acc || managers.includes(id))), false);
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []) => {
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
+};
 
 function countAnimals(species) {
   // seu código aqui
