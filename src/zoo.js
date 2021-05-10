cfb704ea8e4ebaa489090f91c0d4c7e00d2e0a38
Alan Gonçalves <vekio.soft@gmail.com>
2021-05-10 17:24:23 -0300

    conclusão função isManager

--- src/zoo.js
+++ src/zoo.js
@@ -35,9 +35,12 @@ function createEmployee(personalInfo, associatedWith) {
 }
 console.log(createEmployee(['7ed1c9bb-8570-44f6-b718-0666b869573a', 'Criador de gados']));
 
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some(({ managers }) => managers.includes(id));
 }
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
