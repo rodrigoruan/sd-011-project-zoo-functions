5e60718b2eee0ffb29aa2006aa625e59471e33f9
OldRoge <rogeriomrjr@icloud.com>
2021-05-10 09:56:30 -0300

    Requirement 8

--- src/zoo.js
+++ src/zoo.js
@@ -51,7 +51,10 @@ function countAnimals(countSpecies) {
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) return 0;
+  const {Adult = 0, Child = 0, Senior = 0} = entrants;
+  const multiply = (data.prices.Adult * Adult) + (data.prices.Child * Child) + (data.prices.Senior * Senior);
+  return multiply;
 }
 
 function getAnimalMap(options) {
