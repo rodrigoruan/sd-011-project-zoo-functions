c4cf95b5685d47906e866baaf52986a232610f9e
Alan Gonçalves <vekio.soft@gmail.com>
2021-05-10 18:47:21 -0300

    conclusão função addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -40,8 +40,8 @@ function isManager(id) {
 }
 console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({id, firstName, lastName, managers, responsibleFor});
 }
 
 function countAnimals(specie) {
