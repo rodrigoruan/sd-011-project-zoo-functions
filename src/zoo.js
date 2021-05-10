122b3719dd6d05947af03e55bd0719feff773864
Leandro Lopes <leandro_carlos@uol.com.br>
2021-05-10 14:55:41 -0300

    item 10

--- src/zoo.js
+++ src/zoo.js
@@ -8,7 +8,7 @@ eslint no-unused-vars: [
   }
 ]
 */
-
+const { hours } = require('./data')
 const { prices } = require('./data');
 const { species: specie } = require('./data');
 const { employees } = require('./data');
@@ -94,8 +94,25 @@ function getAnimalMap(options) {
   // seu código aqui
 }
 
+function generateText(day) {
+  if (hours[day].close > 12) {
+    hours[day].close -= 12;
+  }
+  if (hours[day].open === 0) {
+    return 'CLOSED';
+  }
+  return `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+}
+
 function getSchedule(dayName) {
-  // seu código aqui
+  const scheduleOfWeek = {};
+  const days = Object.keys(hours);
+  days.forEach((day) => {
+    if (dayName === undefined || dayName === day) {
+      scheduleOfWeek[day] = generateText(day);
+    }
+  });
+  return scheduleOfWeek;
 }
 
 function getOldestFromFirstSpecies(id) {
@@ -105,8 +122,7 @@ function getOldestFromFirstSpecies(id) {
       || specie.name === manager.responsibleFor[0]));
   const oldestAnimal = foundAnimal.residents
     .sort((animal1, animal2) => animal2.age - animal1.age)[0];
-  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
-       
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];       
 }
 
 function increasePrices(percentage) {
