f389d5b67d905f745b8847945643a67e75907aba
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-10 18:43:39 -0300

    Requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -24,7 +24,7 @@ function getAnimalsOlderThan(animal, age) {
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
