e96bb2c314f53e6ea40d0834a37ddf98e5792029
Genivaldo <cgenivaldo46@gmail.com>
2021-05-10 15:53:11 -0300

    Requesito pega

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,11 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function getSpeciesByIds(ids) {
-  return data.species.filter((item) => item.id);
+  return data.species.filter((item) => {
+    const pega = item.id;
+    return pega;
+  });
 }
-console.log(getSpeciesByIds());
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
 }
