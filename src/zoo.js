269f208fd1fcc3974584e67b583baad7fa1f9b5b
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-05-10 18:52:45 -0300

    Realizacao desafio 6

--- src/zoo.js
+++ src/zoo.js
@@ -31,8 +31,8 @@ function isManager(id) {
   return data.employees.some((employees) => employees.managers.some((value) => value === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function countAnimals(species) {
