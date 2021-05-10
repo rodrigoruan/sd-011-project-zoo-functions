bf2ffde011fcee3050c00cac5b6eed842b56a0f7
renan <renan_menezes07@hotmail.com>
2021-05-10 09:30:07 -0300

    Resolução da questão 7.

--- src/zoo.js
+++ src/zoo.js
@@ -9,13 +9,13 @@ eslint no-unused-vars: [
 ]
 */
 
+// const { species } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...idCode) {
   if (!idCode) {
     return [];
   }
-  // let idCodeList = [...idCode];
   const specieList = data.species.filter((specie) => idCode.find((id) => specie.id === id));
   return specieList;
 }
@@ -55,23 +55,28 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function countAnimals(species) {
-  // Contabiliza a quantidade de animais.
-//   data.species.map((specie) => ({ specie.name: specie.resident.length})));
-//   if (!species) {
-//     const specieQuantityList = data.species.reduce((acc, specie) => { acc[`${specie.name}`] = specie.residents.length }, {});
-//     return specieQuantityList;
-//   }
-//   const specieQuantity = data.species.find((specie) => specie.name === species).residents.length;
-//   return specieQuantity;
+// Contabiliza a quantidade de animais.
+  if (!species) {
+    const specieQuantityList = data.species.reduce((acc, specie) => {
+      acc[`${specie.name}`] = specie.residents.length;
+      return acc;
+    }, {});
+    return specieQuantityList;
+  }
+  const specieQuantity = data.species.find((specie) => specie.name === species).residents.length;
+
+  return specieQuantity;
 }
-// console.log(countAnimals('lions'));
 
 function calculateEntry(entrants) {
   // A partir da quantidade de visitantes e a faixa etária de cada um, esta função é responsável por retornar o preço total a ser cobrado.
   // if (!entrants || entrants === {}) return 0;
-  // data.prices.
+
+  // const numberEntrants = Object.entries(entrants);
+  // const totalPaid = numberEntrants.find((ageGroup, index) => )
   // return totalPaid;
 }
+// console.log(calculateEntry({ 'Adult': 2 }));
 
 function getAnimalMap({ includeNames, sorted, sex }) {
   // const filterByRegion = (local) => data.species.filter((specie) => specie.location === local).reduce((animalAcc, specie) => animalAcc.push(specie.name));
@@ -112,47 +117,52 @@ function getSchedule(dayName) {
 
 function getOldestFromFirstSpecies(id) {
   const firstSpecieCaredByEmployee = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+
   const ListOfResidentsCared = data.species.find((specie) => specie.id.includes(firstSpecieCaredByEmployee)).residents;
+
   const ageOfTheOldestResidentCared = ListOfResidentsCared.reduce((oldest, resident) => (oldest > resident.age ? oldest : resident.age), 0);
+
   const getTheOldestResidentCared = ListOfResidentsCared.filter((resident) => resident.age === ageOfTheOldestResidentCared);
 
   return Object.values(getTheOldestResidentCared[0]);
 }
 
 function increasePrices(percentage) {
-  // let { Adult: adult, Senior: senior, Child: child } = data.prices;
+  const pricesByAgeGroup = Object.values(data.prices);
+  const ageGroupList = Object.keys(data.prices);
 
-  // adult *= 1 + percentage / 100;
-  // senior *= 1 + percentage / 100;
-  // child *= 1 + percentage / 100;
+  pricesByAgeGroup.forEach((price, index, arr) => {
+    arr[index] = price * (1 + percentage / 100);
+    arr[index] = Math.round(arr[index] * 100) / 100;
 
-  // let newPriceList = { Adult: 0, Senior: 0, Child: 0 };
-  // newPriceList.Adult = parseFloat(adult.toFixed(2));
-  // newPriceList.Senior = parseFLoat(senior.toFixed(2));
-  // newPriceList.Child = parseFLoat(child.toFixed(2));
-  // return newPriceList;
+    return arr[index];
+  });
+
+  const newPriceList = ageGroupList.reduce((keyAcc, ageGroup, index) => {
+    keyAcc[ageGroup] = pricesByAgeGroup[index];
+    return keyAcc;
+  }, {});
+
+  return newPriceList;
 }
-// console.log(increasePrices(50));
 
 function getEmployeeCoverage(idOrName) {
-  // if (!idOrName) {
-  // const employeeData = Object.values(data.employees);
-  // const specieSupervisioned = data.species.find((animal) => animal.id === employee.responsibleFor[index]);
-//     for (let specieSupervisioned in data.employees.responsibleFor) {
-//       if (specieSupervisioned) {
-//         const specieSupervisionedName = data.species.find((specie) => specie.id === specieSupervisioned).name;
-//       }
-//     }
-//       const employeeReport = data.employees.reduce(
-//           (acc, employee) => {
-//             acc[`${employee.firstName} ${employee.lastName}`] = specieSupervisionedName;
-//             return acc;
-//           }, '');
-//       }
-//     return employeeReport;
-// }
-// console.log(getEmployeeCoverage());
+  // const listOfAnimal = data.species.map((specie) => [specie.id, specie.name]);
+
+  // const fullEmployeeList = data.employees.reduce((acc, employee) => {
+  //   acc[`${employee.firstName} ${employee.lastName}`] = [listOfAnimal];
+  //   return acc;
+  // }, {});
+
+  // const employeeArray = data.employees.filter((employee) => employee.id === idOrName || employee.firstName === idOrName || employee.lastName === idOrName)
+  //   .reduce((acc, employee) => {
+  //     acc[`${employee.firstName} ${employee.lastName}`] = [listOfAnimal];
+  //     return acc;
+  //   }, {});
+
+  // return !idOrName ? fullEmployeeList : employeeArray;
 }
+// console.log(getEmployeeCoverage('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = {
   calculateEntry,
