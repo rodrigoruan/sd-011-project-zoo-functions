f9dde7ffbd4d4a465f784f5ffe5647ae1ceafb43
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-05-10 18:39:22 -0300

    add function getAnimalMap - requeriment 9

--- src/zoo.js
+++ src/zoo.js
@@ -33,7 +33,7 @@ const isManager = (id) => data.employees.some((curr) => curr.managers.includes(i
 const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []) => data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 
 const countAnimals = (species) => {
-  if (species === undefined) {
+  if (!species) {
     return data.species.reduce((acc, curr) => {
       acc[curr.name] = curr.residents.length;
       return acc;
@@ -44,9 +44,28 @@ const countAnimals = (species) => {
 
 const calculateEntry = (entrants) => (entrants ? Object.keys(entrants).reduce((acc, curr) => acc + (entrants[curr] * data.prices[curr]), 0) : 0);
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const getAnimalMap = (options = {}) => {
+  const locations = { NE: [], NW: [], SE: [], SW: [] };
+
+  if (!options.includeNames) {
+    data.species.forEach(({ curr, index }) => locations[curr].push(index));
+    return locations;
+  }
+  if (options.sex) {
+    data.species.forEach(({ curr, index, array }) =>
+      locations[index].push({
+        [curr]: array.filter((animal) => animal.sex === options.sex).map((name) => name.name),
+      }));
+  } else {
+    data.species.forEach(({ curr, index, array }) =>
+      locations[index].push({ [curr]: array.map((element) => element.name) }));
+  }
+  if (options.sorted) {
+    Object.keys(locations).forEach((curr) =>
+      locations[curr].forEach((item) => item[Object.keys(item)].sort()));
+  }
+  return locations;
+};
 
 const getSchedule = (dayName) => {
   const schedules = {};
@@ -77,11 +96,7 @@ const getOldestFromFirstSpecies = (id) => {
   return Object.values(getOlderAgeAnimal[0]);
 };
 
-const increasePrices = (percentage) => {
-  Object.keys(data.prices).forEach((curr) => {
-    data.prices[curr] = Math.ceil(data.prices[curr] * (percentage + 100)) / 100;
-  });
-};
+const increasePrices = (percentage) => Object.keys(data.prices).forEach((curr) => { data.prices[curr] = Math.ceil(data.prices[curr] * (percentage + 100)) / 100; });
 
 const getEmployeeCoverage = (idOrName) => {
   const result = {};
