f97e642cd7a7da0c31394073874cc98865da3c63
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-11 00:51:52 -0300

    Requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -30,7 +30,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.some((idVerify) => idVerify === id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
