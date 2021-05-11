96938eb68fd348604b1bbb154d5538392aa70509
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-05-10 21:17:44 -0300

    Projeto Zoo Functions - Item 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,11 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const animalIds = data.species.filter((specie) => ids.includes(specie.id));
+  return animalIds;
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
