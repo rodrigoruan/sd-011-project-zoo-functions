6ecb1f501a77a6562dc9de21abf44ac2445e0bab
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-05-11 18:15:56 -0300

    Requisito 11 feito

--- src/zoo.js
+++ src/zoo.js
@@ -95,96 +95,96 @@ function calculateEntry(entrants = { Adult: 0, Child: 0, Senior: 0 }) {
   return total;
 }
 
-const defaultAnimalMap = (result) => {
-  if (result === 'lions') return result;
-  let newResult = result;
-  species.forEach((specie) => {
-    if (specie.location === 'NE') newResult.NE.push(specie.name);
-    if (specie.location === 'NW') newResult.NW.push(specie.name);
-    if (specie.location === 'SE') newResult.SE.push(specie.name);
-    if (specie.location === 'SW') newResult.SW.push(specie.name);
-  });
-  return newResult;
-};
+// const defaultAnimalMap = (result) => {
+//   if (result === 'lions') return result;
+//   let newResult = result;
+//   species.forEach((specie) => {
+//     if (specie.location === 'NE') newResult.NE.push(specie.name);
+//     if (specie.location === 'NW') newResult.NW.push(specie.name);
+//     if (specie.location === 'SE') newResult.SE.push(specie.name);
+//     if (specie.location === 'SW') newResult.SW.push(specie.name);
+//   });
+//   return newResult;
+// };
 
-const namedAnimalMap = (result, includeNames) => {
-  if (includeNames !== true || result === 'lions') return result;
-  const { NE, NW, SE, SW } = result;
-  const newResult = { NE: [], NW: [], SE: [], SW: [] };
-  NE.forEach((specieNE, index) => {
-    newResult.NE[index] = { [specieNE]: [] };
-    const specieInfoNE = species.find((specie) => specie.name === specieNE);
-    specieInfoNE.residents.forEach((resident) => newResult.NE[index][specieNE].push(resident.name));
-  });
-  NW.forEach((specieNW, index) => {
-    newResult.NW[index] = { [specieNW]: [] };
-    const specieInfoNW = species.find((specie) => specie.name === specieNW);
-    specieInfoNW.residents.forEach((resident) => newResult.NW[index][specieNW].push(resident.name));
-  });
-  SE.forEach((specieSE, index) => {
-    newResult.SE[index] = { [specieSE]: [] };
-    const specieInfoSE = species.find((specie) => specie.name === specieSE);
-    specieInfoSE.residents.forEach((resident) => newResult.SE[index][specieSE].push(resident.name));
-  });
-  SW.forEach((specieSW, index) => {
-    newResult.SW[index] = { [specieSW]: [] };
-    const specieInfoSW = species.find((specie) => specie.name === specieSW);
-    specieInfoSW.residents.forEach((resident) => newResult.SW[index][specieSW].push(resident.name));
-  });
-  return newResult;
-};
+// const namedAnimalMap = (result, includeNames) => {
+//   if (includeNames !== true || result === 'lions') return result;
+//   const { NE, NW, SE, SW } = result;
+//   const newResult = { NE: [], NW: [], SE: [], SW: [] };
+//   NE.forEach((specieNE, index) => {
+//     newResult.NE[index] = { [specieNE]: [] };
+//     const specieInfoNE = species.find((specie) => specie.name === specieNE);
+//     specieInfoNE.residents.forEach((resident) => newResult.NE[index][specieNE].push(resident.name));
+//   });
+//   NW.forEach((specieNW, index) => {
+//     newResult.NW[index] = { [specieNW]: [] };
+//     const specieInfoNW = species.find((specie) => specie.name === specieNW);
+//     specieInfoNW.residents.forEach((resident) => newResult.NW[index][specieNW].push(resident.name));
+//   });
+//   SE.forEach((specieSE, index) => {
+//     newResult.SE[index] = { [specieSE]: [] };
+//     const specieInfoSE = species.find((specie) => specie.name === specieSE);
+//     specieInfoSE.residents.forEach((resident) => newResult.SE[index][specieSE].push(resident.name));
+//   });
+//   SW.forEach((specieSW, index) => {
+//     newResult.SW[index] = { [specieSW]: [] };
+//     const specieInfoSW = species.find((specie) => specie.name === specieSW);
+//     specieInfoSW.residents.forEach((resident) => newResult.SW[index][specieSW].push(resident.name));
+//   });
+//   return newResult;
+// };
 
-const genreAnimalMap = (result, sex) => {
-  if (result = 'lions') return result;
-  let newResult = result;
-  const { NE, NW, SE, SW } = newResult;
-  const regions = [NE, NW, SE, SW];
-  regions.forEach((region) => {
-    region.forEach((specieRegion, index1) => {
-      const currentSpecie = Object.keys(specieRegion)[0];
-      const specieInfo = species.find((specie) => currentSpecie === specie.name);
-      console.log(specieInfo);
-      let splicePointer = 0;
-      specieInfo.residents.forEach((resident, index2) => {
-        if (resident.sex !== sex) {
-          region[index1][currentSpecie].splice(index2 + splicePointer, 1);
-          splicePointer -= 1;
-        }
-      });
-    });
-  });
-  return newResult;
-};
+// const genreAnimalMap = (result, sex) => {
+//   if (result = 'lions') return result;
+//   let newResult = result;
+//   const { NE, NW, SE, SW } = newResult;
+//   const regions = [NE, NW, SE, SW];
+//   regions.forEach((region) => {
+//     region.forEach((specieRegion, index1) => {
+//       const currentSpecie = Object.keys(specieRegion)[0];
+//       const specieInfo = species.find((specie) => currentSpecie === specie.name);
+//       console.log(specieInfo);
+//       let splicePointer = 0;
+//       specieInfo.residents.forEach((resident, index2) => {
+//         if (resident.sex !== sex) {
+//           region[index1][currentSpecie].splice(index2 + splicePointer, 1);
+//           splicePointer -= 1;
+//         }
+//       });
+//     });
+//   });
+//   return newResult;
+// };
 
-const sortedAnimalMap = (result, sorted) => {
-  if (sorted !== true) return result;
-  let newResult = result;
-  const { NE, NW, SE, SW } = newResult;
-  const regions = [NE, NW, SE, SW];
-  regions.forEach((region) => {
-    region.forEach((specieRegion) => {
-      const animalsNames = Object.values(specieRegion);
-      animalsNames[0].sort();
-    });
-  });
-  return result;
-};
+// const sortedAnimalMap = (result, sorted) => {
+//   if (sorted !== true) return result;
+//   let newResult = result;
+//   const { NE, NW, SE, SW } = newResult;
+//   const regions = [NE, NW, SE, SW];
+//   regions.forEach((region) => {
+//     region.forEach((specieRegion) => {
+//       const animalsNames = Object.values(specieRegion);
+//       animalsNames[0].sort();
+//     });
+//   });
+//   return result;
+// };
 
-function getAnimalMap(options = { includeNames: false, sex: '', sorted: false }) {
-  const { includeNames, sex, sorted } = options;
-  let result = {
-    NE: [],
-    NW: [],
-    SE: [],
-    SW: [],
-  };
-  if (includeNames !== true && (sorted === true || sex === 'male' || sex === 'female')) result = 'lions';
-  result = defaultAnimalMap(result);
-  result = namedAnimalMap(result, includeNames);
-  if (sex === 'male' || sex === 'female') result = genreAnimalMap(result, sex);
-  result = sortedAnimalMap(result, sorted);
-  return result;
-}
+// function getAnimalMap(options = { includeNames: false, sex: '', sorted: false }) {
+//   const { includeNames, sex, sorted } = options;
+//   let result = {
+//     NE: [],
+//     NW: [],
+//     SE: [],
+//     SW: [],
+//   };
+//   if (includeNames !== true && (sorted === true || sex === 'male' || sex === 'female')) result = 'lions';
+//   result = defaultAnimalMap(result);
+//   result = namedAnimalMap(result, includeNames);
+//   if (sex === 'male' || sex === 'female') result = genreAnimalMap(result, sex);
+//   result = sortedAnimalMap(result, sorted);
+//   return result;
+// }
 
 const { hours } = data;
 
@@ -209,10 +209,16 @@ function getSchedule(dayName) {
   return result;
 }
 
-console.log(getSchedule('Tuesday'));
-
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  let result = [];
+  const employeSelected = employees.find((employee) => employee.id === id);
+  const firstSpecie = employeSelected.responsibleFor[0];
+  const specieSelected = species.find((specie) => specie.id === firstSpecie);
+  specieSelected.residents.forEach((resident, index) => {
+    if (index === 0) result = [resident.name, resident.sex, resident.age];
+    if (result[2] < resident.age) result = [resident.name, resident.sex, resident.age];
+  });
+  return result;
 }
 
 function increasePrices(percentage) {
@@ -227,7 +233,7 @@ module.exports = {
   calculateEntry,
   getSchedule,
   countAnimals,
-  getAnimalMap,
+  // getAnimalMap,
   getSpeciesByIds,
   getEmployeeByName,
   getEmployeeCoverage,
