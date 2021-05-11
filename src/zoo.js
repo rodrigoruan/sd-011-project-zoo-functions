95cf2883ea6d23c0a6fb6e55d677a93a6ac95f35
lucasSCsantos <luc.cristovam10@gmail.com>
2021-05-11 02:49:17 -0300

    pega brocougit add .

--- src/zoo.js
+++ src/zoo.js
@@ -12,6 +12,13 @@ eslint no-unused-vars: [
 const data = require('./data');
 // const assert = require('assert');
 
+const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
+const ottersId = '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae';
+const elephantsId = 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5';
+const snakesId = '78460a91-f4da-4dea-a469-86fd2b8ccc84';
+const frogsId = '89be95b3-47e4-4c5b-b687-1fabf2afa274';
+const bearsId = 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d';
+const tigersId = 'e8481c1d-42ea-4610-8e11-1752cfc05a46';
 const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
 const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
 const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
@@ -222,8 +229,6 @@ function getOldestFromFirstSpecies(id) {
   return oldest;
 }
 
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
-
 function increasePrices(percentage) {
   Object.keys(data.prices).forEach((price) => {
     data.prices[price] += data.prices[price] * (percentage / 100);
@@ -231,8 +236,27 @@ function increasePrices(percentage) {
   });
 }
 
+function getSpeciesByIdsReturningName(...ids) {
+  let animal = [];
+  ids.forEach((iD) => animal.push((data.species.find((specie) => specie.id === iD)).name));
+  return animal;
+}
+
 function getEmployeeCoverage(idOrName) {
-  // seu código aqui
+  let coverage = {};
+  let coverageSimple = {};
+  data.employees.forEach((employe) => {
+    const employeResponsibles = getSpeciesByIdsReturningName(...employe.responsibleFor);
+    coverage[`${employe.firstName} ${employe.lastName}`] = employeResponsibles;
+    if (employe.firstName === idOrName || employe.lastName === idOrName || employe.id === idOrName) {
+      coverageSimple = { [`${employe.firstName} ${employe.lastName}`]: employeResponsibles };
+    }
+  });
+
+  if (!idOrName) {
+    return coverage;
+  }
+  return coverageSimple;
 }
 
 module.exports = {
