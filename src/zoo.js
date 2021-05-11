047dc12c67cc956e29f0f2632a9c3d3792da43d3
Matheus Costa <mathws.costag@gmail.com>
2021-05-10 21:58:46 -0300

    cria a funcao calculateEntry

--- src/zoo.js
+++ src/zoo.js
@@ -13,33 +13,32 @@ const data = require('./data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  const find = (SpeciesId) => data.species.find((element) => element.id === SpeciesId);
-  const mapIds = (array) => array.map((id) => find(id));
-  return mapIds(ids);
+  const filterIds = data.species.filter(({ id }) => ids.includes(id));
+  return filterIds;
 }
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const findResidents = data.species.find((element) => element.name === animal).residents;
-  const checkAge = findResidents.every((element) => (element.age >= age));
+  const findResidents = data.species.find(({ name }) => name === animal).residents;
+  const checkAge = findResidents.every((resident) => (resident.age >= age));
   return checkAge;
 }
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
-  const findEmployee = data.employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
-  return (employeeName === undefined) ? {} : findEmployee;
+  const findEmployee = data.employees.find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+  return (!employeeName) ? {} : findEmployee;
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
-  return Object.assign(personalInfo, associatedWith);
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
   // seu código aqui
-  const mapEmployee = data.employees.map((element) => element.managers.some((manege) => manege === id));
-  return mapEmployee.some((element) => element === true);
+  const mapEmployee = data.employees.some(({ managers }) => managers.includes(id));
+  return mapEmployee;
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
@@ -55,17 +54,17 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 
 function countAnimals(species) {
   // seu código aqui
-  const allSpecies = data.species.reduce(((curObject, accSpecie) => {
-    curObject[accSpecie.name] = accSpecie.residents.length;
+  const allSpecies = data.species.reduce(((curObject, { name, residents }) => {
+    curObject[name] = residents.length;
     return curObject;
   }), {});
-  return (species === undefined) ? allSpecies : data.species.find((element) => element.name === species).residents.length;
+
+  return (!species) ? allSpecies : data.species.find((element) => element.name === species).residents.length;
 }
 
-console.log(countAnimals());
-
-function calculateEntry(entrants) {
+function calculateEntry({ Adult = 0, Senior = 0, Child = 0 } = 0) {
   // seu código aqui
+  return ((Adult * 49.99) + (Senior * 24.99) + (Child * 20.99));
 }
 
 function getAnimalMap(options) {
