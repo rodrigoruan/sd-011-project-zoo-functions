44d4d764b26cef25a570f61c5e19f2ffe7104687
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-05-10 21:16:28 -0300

    Requisito 6 feitot

--- src/zoo.js
+++ src/zoo.js
@@ -62,8 +62,14 @@ function isManager(id) {
   return result;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees = [...data.employees, {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  }];
 }
 
 function countAnimals(otherspecies) {
