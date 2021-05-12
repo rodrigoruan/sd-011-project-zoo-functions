3ec449dd2b49859a13c1fc6fe1c83b4e90d2443c
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-05-12 09:31:06 -0300

    getAnimalsOlderThan

--- src/zoo.js
+++ src/zoo.js
@@ -18,6 +18,7 @@ function getSpeciesByIds(...ids) {
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return data.species.find(((specie) => specie.name === animal)).residents.every((species) => species.age >= age);
 }
 
 function getEmployeeByName(employeeName) {
