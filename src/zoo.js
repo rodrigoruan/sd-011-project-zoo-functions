e6824baee485d40bc63e785c3260c4a51ee9bddb
victordg00 <victord.goncalves@outlook.com>
2021-05-13 17:39:57 -0300

    requisito 1 feito

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
-  return data.species.filter((Species) => ids.some((id) => Species.id === id));
+    return data.species.filter((Species) => ids.some((id) => Species.id === id));
 }
 
 function getAnimalsOlderThan(animal, age) {
