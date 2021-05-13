a455d3a2dbc36cba64b92ca37fcd83858292ab78
guinhoanjos <iago.anjos@hotmail.com>
2021-05-13 19:16:05 -0300

    Update zoo.js

--- src/zoo.js
+++ src/zoo.js
@@ -118,9 +118,9 @@ function getOldestFromFirstSpecies(id) {
 
 function increasePrices(percentage) {
   let porcentagem = percentage / 100;
-  data.prices.Adult = (Math.round((prices.Adult + (prices.Adult * porcentagem)) * 100)) / 100;
-  data.prices.Child = (Math.round((prices.Child + (prices.Child * porcentagem)) * 100)) / 100;
-  data.prices.Senior = (Math.round((prices.Senior + (prices.Senior * porcentagem)) * 100)) / 100;
+  prices.Adult = (Math.round((prices.Adult + (prices.Adult * porcentagem)) * 100)) / 100;
+  prices.Child = (Math.round((prices.Child + (prices.Child * porcentagem)) * 100)) / 100;
+  prices.Senior = (Math.round((prices.Senior + (prices.Senior * porcentagem)) * 100)) / 100;
   return prices;
 }
 
