2da38b4acb2d5ed4c001c08dc26ac7f8423c9b6b
Rafael Schmitz <schimitaum@gmail.com>
2021-05-14 01:31:39 -0300

    req 12

--- src/zoo.js
+++ src/zoo.js
@@ -9,15 +9,14 @@ eslint no-unused-vars: [
 ]
 */
 
-const { employees, species, prices, hours } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
-  return species.filter((specie) => ids.some((value) => value === specie.id));
+  return data.species.filter((specie) => ids.some((value) => value === specie.id));
 }
 
 function getAnimalsOlderThan(animal, age) {
-  return species.find((specie) => specie.name === animal)
+  return data.species.find((specie) => specie.name === animal)
     .residents
     .every((resident) => resident.age > age);
 }
@@ -26,7 +25,7 @@ function getEmployeeByName(employeeName) {
   if (!employeeName) {
     return {};
   }
-  return employees.find((employee) => (employee.firstName === employeeName || employee.lastName === employeeName));
+  return data.employees.find((employee) => (employee.firstName === employeeName || employee.lastName === employeeName));
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -34,11 +33,11 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  return employees.some(({ managers }) => managers.includes(id));
+  return data.employees.some(({ managers }) => managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  employees.push({ id, firstName, lastName, managers, responsibleFor });
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function countAnimals(Species) {
@@ -47,10 +46,10 @@ function countAnimals(Species) {
     const criaObjeto = (animal) => {
       speciesTotals[animal.name] = animal.residents.length;
     };
-    species.forEach(criaObjeto);
+    data.species.forEach(criaObjeto);
     return speciesTotals;
   }
-  return species.find((specie) => specie.name === Species).residents.length;
+  return data.species.find((specie) => specie.name === Species).residents.length;
 }
 
 function calculateEntry(entrants) {
@@ -58,7 +57,7 @@ function calculateEntry(entrants) {
     return 0;
   }
   const people = Object.entries(entrants); // Entrants não é array
-  const totalizer = (acc, cur) => acc + (cur[1] * prices[cur[0]]);
+  const totalizer = (acc, cur) => acc + (cur[1] * data.prices[cur[0]]);
   return people.reduce(totalizer, 0);
 }
 
@@ -67,7 +66,7 @@ function getAnimalMap(options) {
 }
 
 function getSchedule(dayName) {
-  let week = Object.entries(hours); // HOURS NÃO È ARRAY
+  let week = Object.entries(data.hours); // HOURS NÃO È ARRAY
   if (dayName) {
     week = week.filter((item) => item[0] === dayName);
   }
@@ -83,15 +82,20 @@ function getSchedule(dayName) {
 }
 
 function getOldestFromFirstSpecies(id) {
-  const firstSpecieId = employees.find((employee) => employee.id === id).responsibleFor[0];
-  const specieResidents = species.find((specie) => specie.id === firstSpecieId).residents;
+  const firstSpecieId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const specieResidents = data.species.find((specie) => specie.id === firstSpecieId).residents;
   const oldest = specieResidents.sort((a, b) => (b.age > a.age ? 1 : -1))[0];
   const { name, sex, age } = oldest;
   return [name, sex, age];
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const value = Object.entries(data.prices);
+  const multiply = (acc, cur) => {
+    acc[cur[0]] = Math.ceil((100 + percentage) * cur[1]) / 100;
+    return acc;
+  };
+  data.prices = value.reduce(multiply, {});
 }
 
 function getEmployeeCoverage(idOrName) {
