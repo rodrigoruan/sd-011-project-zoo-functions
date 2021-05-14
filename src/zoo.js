d528f66a781187d0f592662214a508d123889f99
JoaoCabrera <joaocabrerati@gmail.com>
2021-05-14 07:56:41 -0300

    Adiciona requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -77,7 +77,18 @@ function getAnimalMap(options) {
 }
 
 function getSchedule(dayName) {
-  // seu código aqui
+  const days = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+  if (dayName !== undefined) {
+    return days[dayName];
+  } return days;
 }
 
 function getOldestFromFirstSpecies(id) {
