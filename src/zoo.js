68e6b506a541318f855fda7e1a41347eb670817d
Taís Carvalho <taispcarvalho@gmail.com>
2021-05-14 09:20:19 -0300

    5 e 6

--- src/zoo.js
+++ src/zoo.js
@@ -31,12 +31,22 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id))
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return employees.push(
+    {
+      id: id,
+      firstName: firstName,
+      lastName: lastName,
+      managers: managers,
+      responsibleFor: responsibleFor,
+    }
+  )
 }
+
 
 function countAnimals() {
   // seu código aqui
