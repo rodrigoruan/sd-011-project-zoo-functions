50b51bbd2898f17b986fea098b4d95cfd36b265b
urielbochi <urielbochi@gmail.com>
2021-05-13 22:00:24 -0300

    Tentativa correção LINT

--- src/zoo.js
+++ src/zoo.js
@@ -10,6 +10,7 @@ eslint no-unused-vars: [
 */
 
 const { species, employees, prices, hours } = require('./data');
+const data = require('./data');
 
 function getSpeciesByIds(...ids) {
   return species.filter((animals) => ids.includes(animals.id));
@@ -26,7 +27,6 @@ function getEmployeeByName(employeeName) {
   if (!employeeName) return {};
 
   return employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
-
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -34,40 +34,36 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
+  return employees.some((employee) => employee.managers.includes(id));
+}
+
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
-
-//function countAnimals(species) {
-  // seu código aqui
-//}
+function countAnimals(specie) {
+  }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+
 }
 
 function getAnimalMap(options) {
-  // seu código aqui
 }
 
 function getSchedule(dayName) {
-  // seu código aqui
 }
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+
 }
 
 function getEmployeeCoverage(idOrName) {
-  // seu código aqui
-}
+  }
+
 
 module.exports = {
   calculateEntry,
