11a9ade3b42dcc4193484752208fa70b79d9aa74
igordmf <igordanielmf@gmail.com>
2021-05-13 21:37:41 -0300

    Adiciona requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -47,8 +47,16 @@ function isManager(id) {
   return data.employees.some((employee) => employee.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  const employee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor
+  };
+  data.employees.push(employee);
 }
 
 function countAnimals(species) {
