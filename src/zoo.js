3068d9d473967d14dff4db425b1374593e977f77
Nalu <aluziapantoja@gmail.com>
2021-05-15 17:48:15 -0300

    Req 6

--- src/zoo.js
+++ src/zoo.js
@@ -32,8 +32,8 @@ function isManager(id) {
   return data.employees.some((emp) => emp.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function countAnimals(species) {
