d29850cb0986a8bc555e5e1896acb2aa222457ad
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-05-14 21:41:16 -0300

    Projeto Zoo Functions - Envio Itens 4 a 7

--- src/zoo.js
+++ src/zoo.js
@@ -38,37 +38,57 @@ console.log(getAnimalsOlderThan('penguins', 10));
 // solução com o apoio da aula de revisão de Trybers e consultas aos sites:
 // https://www.youtube.com/watch?v=rAzHvYnQ8DY
 // https://www.youtube.com/watch?v=nYRIRZBHQ3s
-const { employees } = require('./data');
+// const { employees } = require('./data');
 
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
   }
-  const EmployeeData = employees.find((employee) => employeeName === employee.firstName || employeeName === employee.lastName);
-  return EmployeeData;
+  const employeeData = data.employees.find((employee) => employeeName === employee.firstName || employeeName === employee.lastName);
+  return employeeData;
 }
-// const EmployeeLastName = employees.find(employee => employeeName === employee.lastName);
-// return EmployeeLastName;
-// }
+
 console.log(getEmployeeByName());
 console.log(getEmployeeByName('Emery'));
 console.log(getEmployeeByName('Wishart'));
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return {
+    ...personalInfo,
+    ...associatedWith,
+  };
 }
+
+const { employees } = require('./data');
 
 function isManager(id) {
-  // seu código aqui
+  // const checkManager = employees.some((employee) => id === employees.id);
+  const checkPerson = employees.some((employee) => (id === employees.id || employee.managers.includes(id)));
+  return checkPerson;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = {
+    id, firstName, lastName, managers, responsibleFor,
+  };
+  return employees.push(newEmployee);
 }
 
 function countAnimals(species) {
-  // seu código aqui
+  const animalQtd = data.species.reduce((acc, { name, residents }) => {
+    acc[name] = residents.length;
+    return acc;
+  }, {});
+
+  if (!species) {
+    return animalQtd;
+  }
+  return animalQtd[species];
 }
+console.log(countAnimals());
 
 function calculateEntry(entrants) {
   // seu código aqui
