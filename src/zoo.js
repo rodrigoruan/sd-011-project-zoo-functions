d6c28b8d4d24bff2393ed3b4414c4e6f0c90d82e
victordg00 <victord.goncalves@outlook.com>
2021-05-16 12:04:29 -0300

    vamo pra oitava :v

--- src/zoo.js
+++ src/zoo.js
@@ -56,46 +56,46 @@ function addEmployee(id, firstName, lastName, managers=[], responsibleFor=[]) {
   return data.employees;
 }
 
-// function countAnimals(species) {
-//   // seu código aqui
-// }
+function countAnimals(species) {
+  // seu código aqui
+}
 
-// function calculateEntry(entrants) {
-//   // seu código aqui
-// }
+function calculateEntry(entrants) {
+  // seu código aqui
+}
 
-// function getAnimalMap(options) {
-//   // seu código aqui
-// }
+function getAnimalMap(options) {
+  // seu código aqui
+}
 
-// function getSchedule(dayName) {
-//   // seu código aqui
-// }
+function getSchedule(dayName) {
+  // seu código aqui
+}
 
-// function getOldestFromFirstSpecies(id) {
-//   // seu código aqui
-// }
+function getOldestFromFirstSpecies(id) {
+  // seu código aqui
+}
 
-// function increasePrices(percentage) {
-//   // seu código aqui
-// }
+function increasePrices(percentage) {
+  // seu código aqui
+}
 
-// function getEmployeeCoverage(idOrName) {
-//   // seu código aqui
-// }
+function getEmployeeCoverage(idOrName) {
+  // seu código aqui
+}
 
 module.exports = {
-  // calculateEntry,
-  // getSchedule,
-  // countAnimals,
-  // getAnimalMap,
+  calculateEntry,
+  getSchedule,
+  countAnimals,
+  getAnimalMap,
   getSpeciesByIds,
   getEmployeeByName,
-  // getEmployeeCoverage,
+  getEmployeeCoverage,
   addEmployee,
   isManager,
   getAnimalsOlderThan,
-  // getOldestFromFirstSpecies,
-  // increasePrices,
+  getOldestFromFirstSpecies,
+  increasePrices,
   createEmployee,
 };
