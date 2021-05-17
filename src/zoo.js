cb56049b0dd5080d7852067de0a1cecfbd7f859f
Luana Souza <luanah.souzza@hotmail.com>
2021-05-17 12:02:19 -0300

    Corrige erro Requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -39,7 +39,7 @@ function isManager(id) {
 }
 console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
-function addEmployee(id, firstName, lastName, managers = [], responsibleFor = ) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = [ ]) {
   data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
