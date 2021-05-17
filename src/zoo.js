b6a9116074d3c29f69ca476cb05d182b624ac9a7
Thiago Gomes <thiagog.macedom@gmail.com>
2021-05-16 22:08:20 -0300

    10 requisito

--- src/zoo.js
+++ src/zoo.js
@@ -64,14 +64,27 @@ function calculateEntry(entrants) {
   return value;
 }
 
-function getAnimalMap(options) {
-  // seu código aqui
+function getAnimalMap() {
 }
 
-function getSchedule(dayName) {
-  // seu código aqui
+function getSchedule(day) {
+  const days = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+  if (day) {
+    const dictTemp = {};
+    dictTemp[day] = days[day];
+    return dictTemp;
+  }
+  return days;
 }
-
+console.log(getSchedule('Monday'));
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
 }
