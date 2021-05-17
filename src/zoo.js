4e608717bfb77a08451fa06a6fcd28604de0df2f
Rafael-oliveira <rafa_batista02@hotmail.com>
2021-05-17 09:48:42 -0300

    9,10,11

--- src/zoo.js
+++ src/zoo.js
@@ -10,6 +10,7 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const { employees } = require('./data');
 // const { species } = require('./data');
 
 function getSpeciesByIds(...ids) {
@@ -54,24 +55,66 @@ function calculateEntry(entrants) {
   return Object.keys(entrants).reduce((acc, value) => acc + (data.prices[value] * entrants[value]), 0);
 }
 
-function getAnimalMap(options) {
-  // seu código aqui
+function getAnimalMap(options = {}) {
+  let result = {};
+  let arrayRegions = ['NE', 'NW', 'SE', 'SW'];
+  arrayRegions.forEach((regions) => {
+    result[regions] = [];
+  });
+  data.species.map((animal) => (options.includeNames ? result[animal.location].push({ [animal.name]: getNameAnimal(animal.residents, options.sorted, options.sex) }) : result[animal.location].push(animal.name)));
+  return result;
 }
 
 function getSchedule(dayName) {
-  // seu código aqui
+  const result = {};
+  if (dayName && dayName !== 'Monday') {
+    result[dayName] = `Open from ${data.hours[dayName].open}am until ${data.hours[dayName].close - 12}pm`;
+    return result;
+  } if (dayName === 'Monday') {
+    return { Monday: 'CLOSED' };
+  }
+  return {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED' };
 }
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalId = data.employees.filter((ids) => ids.id === id)[0].responsibleFor[0];
+  const searchAnimal = data.species.filter((value) => value.id === animalId)[0].residents;
+  const ageAnimalsAll = searchAnimal.reduce((acc, list) => Math.max(acc, list.age), 0);
+  const result = searchAnimal.find((age) => age.age === ageAnimalsAll);
+  return [result.name, result.sex, result.age];
 }
 
 function increasePrices(percentage) {
   // seu código aqui
+  const adultPrice = ((data.prices.Adult / 100) * percentage + data.prices.Adult + 0.001);
+  const childPrice = ((data.prices.Child / 100) * percentage + data.prices.Child + 0.001);
+  const seniorPrice = ((data.prices.Senior / 100) * percentage + data.prices.Senior + 0.001);
+
+  data.prices.Adult = Number(adultPrice.toFixed(2));
+  data.prices.Senior = Number(seniorPrice.toFixed(2));
+  data.prices.Child = +childPrice.toFixed(2);
 }
 
 function getEmployeeCoverage(idOrName) {
   // seu código aqui
+  const consultAnimalEmployee = {};
+  if (!idOrName) {
+    employees.forEach((value) => {
+      consultAnimalEmployee[`${value.firstName} ${value.lastName}`] = value.responsibleFor.map((valor) => data.species.find((specie) => specie.id === valor).name);
+    });
+    return consultAnimalEmployee;
+  }
+  data.employees.filter((employee) => employee.id === idOrName || employee.firstName === idOrName || employee.lastName === idOrName).forEach((value) => {
+    consultAnimalEmployee[`${value.firstName} ${value.lastName}`] = value.responsibleFor.map((valor) => data.species.find((specie) => specie.id === valor).name);
+  });
+  return consultAnimalEmployee;
 }
 
 module.exports = {
